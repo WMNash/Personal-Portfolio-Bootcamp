6052ec582f99fc1d00ddcec793aaaa0f809dfe6a
WMNash <wanmuhdnasrullah@gmail.com>
2023-04-08 07:30:42 +0800

    third commit

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-
 // Assigning value to JS
-let btnDOM = document.querySelector('inputField-btn')
+let btnDOM = document.querySelector('.inputField-btn')
 
 
+btnDOM.addEventListener('click', (event) => {
+    console.log(event);
+})
+
+
+
+document.addEventListener('keypress', (event) => {
+    let keyCode = event.key ? event.key : event.which;
+
+    if (keyCode === 13) {
+        btnDOM.click()
+    }
+})
