8a847ffd19aebb2f3e270011f2c7eb9a79a6e76a
WMNash <wanmuhdnasrullah@gmail.com>
2023-04-08 08:31:19 +0800

    third commit

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 let btnDOM = document.querySelector('.inputField-btn')
 let inputFieldDOM = document.querySelector('.inputField')
 
-btnDOM.addEventListener('keyup', (e) => {
-    e.preventDefault();
-    if (e.keyCode === 13) {
-        btnDOM.click();
-    }
-})
 
 
+function handleMe() {
 
+    btnDOM.addEventListener('keyup', (e) => {
+        e.preventDefault();
+        if (e.keyCode === 13) {
+            btnDOM.click();
+        }
+    })
 
-
-function handleMe()
+}
